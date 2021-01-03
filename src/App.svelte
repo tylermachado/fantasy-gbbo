a1b7a885425cdbd304391e223da1be58521344fb
tylermachado <3586466+tylermachado@users.noreply.github.com>
2021-01-02 22:01:11 -0500

    into the bin

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,135 @@
 <script>
-	export let name;
+
+	function sorter( a, b, key ) {
+	  if ( a[key] < b[key] ){
+	    return -1;
+	  }
+	  if ( a[key] > b[key] ){
+	    return 1;
+	  }
+	  return 0;
+	}
+
+	$: bakers = [
+		{
+			name: "John",
+			points: 0,
+			active: true
+		},
+		{
+			name: "Ringo",
+			points: 0,
+			active: true
+		},
+		{
+			name: "George",
+			points: 0,
+			active: true
+		},
+		{
+			name: "Paul",
+			points: 0,
+			active: true
+		}
+	];
+
+	export let actions = [
+		{
+			action: "Handshake",
+			points: 500
+		},
+		{
+			action: "Judges loom in background",
+			points: 100
+		},
+		{
+			action: "Baker has actually heard of technical",
+			points: 200
+		},
+		{
+			action: "STAR BAKER",
+			points: 1000
+		},
+		{
+			action: "Baker earns 1st place in technical",
+			points: 300
+		},
+		{
+			action: "Prue compliments booze content",
+			points: 200
+		},
+		{
+			action: "Ingredient sampling",
+			points: 100
+		},
+		{
+			action: "Happy tears",
+			points: 300
+		},
+		{
+			action: "Into the bin",
+			points: -300
+		},
+		{
+			action: "\"DISASTER\"",
+			points: -100
+		},
+		{
+			action: "Baker restarts bake",
+			points: -200
+		},
+		{
+			action: "\"Soggy bottom\"",
+			points: -200
+		},
+		{
+			action: "Baker finishes last in technical",
+			points: -300
+		},
+		{
+			action: "Bake too spicy",
+			points: -100
+		},
+		{
+			action: "Sad tears",
+			points: -300
+		},
+		{
+			action: "Baker eliminated",
+			points: -1000
+		}
+	]
+
+	$: bakers = bakers.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
+	actions = actions.sort((a,b) => b["points"]-a["points"])
+
+	function updateScore(action, baker) {
+		baker.points = (baker.points + action.points);
+		bakers = bakers;
+		console.log(baker.name + ": " + baker.points);
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>FANTASY GBBO</h1>
+	{#each bakers as baker}
+		{#if baker.active === true}
+			<div>
+				{baker.name}<br/>
+				{baker.points}
+			</div>
+		{/if}
+	{/each}
+
+	{#each actions as action}
+		<h3>{action.action} ({action.points})</h3>
+		{#each bakers as baker}
+			{#if baker.active === true}
+				<button on:click={updateScore(action, baker)}>{baker.name}</button>
+			{/if}
+		{/each}
+	{/each}
+
 </main>
 
 <style>
